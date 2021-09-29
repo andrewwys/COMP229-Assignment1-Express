618f8a5555a2ef62392a50f003f7cf06fea05377
andrewwy <andrewsit.wy@gmail.com>
2021-09-29 03:08:33 -0400

    services and project page completed

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* Routes navigation for all 5 pages. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
