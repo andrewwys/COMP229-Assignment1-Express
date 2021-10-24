e0f3daff6d15499cd22dc665be13cb51aab42b50
andrewwy <andrewsit.wy@gmail.com>
2021-10-24 16:18:02 -0400

    changed db URI to mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/my_express_site"
+    // "URI": "mongodb://localhost/my_express_site",
+    "URI" : "mongodb+srv://andrew:dorsi8@cluster0.vbnqo.mongodb.net/my_express_site?retryWrites=true&w=majority"
 }
